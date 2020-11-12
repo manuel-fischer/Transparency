1af5aae123012615ac3abce9a0b47693e90446ee
Manuel Fischer <58823087+manuel-fischer@users.noreply.github.com>
2020-11-12 14:53:03 +0100

    Blanking out an image by setting the alpha channel to 0, by reverting the process a seemingly empty image can contain a full rgb image

---
+++ hide.py
@@ -0,0 +1,19 @@
+from PIL import Image
+from sys import argv
+
+
+def hide(img):
+    img = img.convert("RGBA")#.copy()
+    dat = img.load()
+    for y in range(img.height):
+        for x in range(img.width):
+            r,g,b,a = dat[x,y]
+            dat[x,y] = r,g,b,0
+            
+    return img
+
+if __name__=="__main__":
+    PLAIN = argv[1] if len(argv)>1 else "tst/plain_secret.png"
+    CIPHER = argv[2] if len(argv)>2 else PLAIN[:PLAIN.rfind(".")]+".cipher.png"
+    hide(Image.open(PLAIN)).save(CIPHER)
+
---
+++ reveal.py
@@ -0,0 +1,19 @@
+from PIL import Image
+from sys import argv
+
+
+def hide(img):
+    img = img.convert("RGBA")#.copy()
+    dat = img.load()
+    for y in range(img.height):
+        for x in range(img.width):
+            r,g,b,a = dat[x,y]
+            dat[x,y] = r,g,b,255
+            
+    return img
+
+if __name__=="__main__":
+    CIPHER = argv[1] if len(argv)>1 else "tst/plain_secret.cipher.png"
+    PLAIN = argv[2] if len(argv)>2 else CIPHER[:CIPHER.rfind(".")]+".plain.png"
+    hide(Image.open(CIPHER)).save(PLAIN)
+
